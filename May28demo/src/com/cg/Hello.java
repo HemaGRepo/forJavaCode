be7f3334ab95188baeb2879921cb83295189a5de
Hema G <hema.g@capgemini.com>
2018-05-28 17:00:38 +0530

    V5.9

--- May28demo/src/com/cg/Hello.java
+++ May28demo/src/com/cg/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Thank U");
 		System.out.println("Commit to Local....Push to remote");
+		System.out.println("Take it for build.......");
 	}
 
 }
